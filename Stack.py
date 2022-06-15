7535ae66c358d0588286dd4208bc46cae248aec4
Zephan Wong <apeacadamyy@gmail.com>
2022-06-15 10:09:00 +0800

    some odin trials

--- Stack.py
+++ Stack.py
@@ -1,7 +1,7 @@
 browsing = []
-browsing.append(1)
-browsing.append(2)
-browsing.append(3)
+browsing.append('1')
+browsing.append('2')
+browsing.append('3')
 print(browsing)
-last = browsing.pop()
+last = "".join(browsing)
 print(last, browsing)
